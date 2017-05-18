3854460a43b85ee1cb542a58ab2ca4168a941d73
caco <caco@member.fsf.org>
2017-05-18 20:40:38 -0300

    Constructs WordFinder skeleton

---
+++ ep09/WordFinder.java
@@ -0,0 +1,29 @@
+public class WordFinder {
+    // state variables
+    String [] arr;
+    
+    // constructor
+    public WordFinder(String[] arr) {
+        this.arr = arr;
+    }
+    
+    // word that appear the most in given strings
+    public String getMax() {
+        return "bla"; // implement it!
+    }
+    
+    // word that appear in a and b indexes strings
+    public String containedIn(int a, int b) {
+        return "bla"; // implement it!
+    }
+    
+    // array of string array indexes where string appears
+    public int[] appearsIn(String s) {
+        return new int[1]; // implement it!
+    }
+    
+    // unit testing
+    public static void main(String args[]) {
+        
+    }
+}
