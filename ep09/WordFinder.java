5bdfbeaf3f589ca7d3e881d4aa9955be5a4e2d53
caco <caco@member.fsf.org>
2017-05-18 21:04:03 -0300

    First test

--- ep09/WordFinder.java
+++ ep09/WordFinder.java
@@ -24,6 +24,17 @@ public class WordFinder {
     
     // unit testing
     public static void main(String args[]) {
-        
+        // construct WordFinder and make default tests
+        String[] lines = {"Globo atira Michel Temer ao mar",
+            "um ano depois de colocalo no poder por meio de um golpe mandrake",
+            "emissora da familia Marinho deixa claro no Jornal Nacional",
+            "que Temer virou bagaço de laranja noticiario tambem exibe os",
+            "audios em que Temer avaliza a compra do silencio de Eduardo Cunha",
+            "Globo ja prepara seu plano B para substituir Michel Temer e",
+            "encontrar outro nome menos sujo para levar adiante sua agenda",
+            "primeira fala de William Bonner foi sobre a fala nada republicana",
+            "de Temer"};
+        WordFinder wf = new WordFinder(lines);
+        assert wf.arr[8] == "de Temer";
     }
 }
