bb3e877c2d0eb79380d2ae4eb639f44758c92171
caco <caco@member.fsf.org>
2017-06-18 10:07:32 -0300

    Set stage to start TDD

--- ep14/SeamCarver.java
+++ ep14/SeamCarver.java
@@ -1,3 +1,5 @@
+import edu.princeton.cs.algs4.Picture;
+
 public class SeamCarver {
     // create a seam carver object based on the given picture
     public SeamCarver(Picture picture) {
@@ -5,26 +7,35 @@ public class SeamCarver {
     
     // current picture
     public Picture picture() {
+        return new Picture(4, 3);  // implement it!
     }
     
     // width of current picture
     public int width() {
+        return -1;  // implement it!
     }
     
     // height of current picture
     public int height() {
+        return -1;  // implement it!
     }
     
     // energy of pixel at column x and row y
     public double energy(int x, int y) {
+        return -1.0;  // implement it!
     }
     
     // sequence of indices for horizontal seam
     public int[] findHorizontalSeam() {
+        int[] h = {-1};  // implement it!
+        return h;  // implement it!
     }
     
     // sequence of indices for vertical seam
     public int[] findVerticalSeam() {
+        int[] v = {-1};  // implement it!
+        return v;  // implement it!
+        
     }
     
     // remove horizontal seam from current picture
