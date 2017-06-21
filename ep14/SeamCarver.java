3925fcd58d9681b2c4dd58d3a2ab80e92ca72a1f
caco <caco@member.fsf.org>
2017-06-21 20:46:44 -0300

    Find vertical seam, with debugs commented

--- ep14/SeamCarver.java
+++ ep14/SeamCarver.java
@@ -1,10 +1,13 @@
 import edu.princeton.cs.algs4.Picture;
+import edu.princeton.cs.algs4.BST;
+import edu.princeton.cs.algs4.Stack;
 import java.awt.Color;
 import edu.princeton.cs.algs4.StdOut;  // DEBUG
+import java.util.Arrays; // for unit tests
 
 public class SeamCarver {
     // state variables
-    private Picture picture; // we work with a copy of picture passed pby client
+    private Picture picture; // we work with a copy of picture passed by client
     private double[][] energyMatrix;
     
     // create a seam carver object based on the given picture
@@ -12,7 +15,6 @@ public class SeamCarver {
         if (picture == null)
             throw new NullPointerException("Constructor called with null argument");
         this.picture = picture;
-        setEnergyMatrix();
     }
     
     // current picture
@@ -47,9 +49,10 @@ public class SeamCarver {
     
     // sequence of indices for vertical seam
     public int[] findVerticalSeam() {
-        int[] v = {-1};  // implement it!
-        return v;  // implement it!
-        
+        setEnergyMatrix();
+        double[][] pathsMatrix;
+        pathsMatrix = relaxVertices();
+        return seam(pathsMatrix);
     }
     
     // remove horizontal seam from current picture
@@ -70,6 +73,106 @@ public class SeamCarver {
                 energyMatrix[i][j] = energy(j, i);
             }
         }
+    }
+    
+    private double[][] relaxVertices() {
+        double[][] pathsMatrix = new double[height()][width()];
+//        // DEBUG: print energyMatrix
+//        for (int i = 0; i < height(); i++) {
+//            for (int j = 0; j < width(); j++) {
+//                StdOut.print(energyMatrix[i][j] + " ");
+//            }
+//            StdOut.println();
+//        }
+//        StdOut.println();
+        
+        // Initialize pathsMatrix
+        for (int j = 0; j < width(); j++)
+            pathsMatrix[0][j] = energyMatrix[0][j];
+        for (int i = 1; i < height(); i++) {
+            for (int j = 0; j < width(); j++) {
+                pathsMatrix[i][j] = Double.POSITIVE_INFINITY;
+            }
+        }
+        
+//        // DEBUG: print pathsMatrix
+//        for (int i = 0; i < height(); i++) {
+//            for (int j = 0; j < width(); j++) {
+//                StdOut.print(pathsMatrix[i][j] + " ");
+//            }
+//            StdOut.println();
+//        }
+        
+        // Relax vertices (requires building energyMatrix before);
+        int jIndexLeft = 0, jIndexRight = 0;
+        for (int i = 0; i < height() - 1; i++) {
+            for (int j = 0; j < width(); j++) {
+                jIndexLeft = (j-1 == -1 ? width()-1 : j-1);
+                jIndexRight = (j+1 == width() ? 0 : j+1);
+                if (pathsMatrix[i][j] + energyMatrix[i+1][jIndexLeft] < pathsMatrix[i+1][jIndexLeft]) { 
+                    pathsMatrix[i+1][jIndexLeft] = pathsMatrix[i][j] + energyMatrix[i+1][jIndexLeft];
+                }
+//                if (j == 1) break;// DEBUG
+                if (pathsMatrix[i][j] + energyMatrix[i+1][j] < pathsMatrix[i+1][j]) {
+                    pathsMatrix[i+1][j] = pathsMatrix[i][j] + energyMatrix[i+1][j];
+                }
+                if (pathsMatrix[i][j] + energyMatrix[i+1][jIndexRight] < pathsMatrix[i+1][jIndexRight]) {
+                    pathsMatrix[i+1][jIndexRight] = pathsMatrix[i][j] + energyMatrix[i+1][jIndexRight];
+                }
+            }
+//            if (i == 0) break;  // DEBUG
+        }
+        
+//        // DEBUG: print pathsMatrix
+//        for (int i = 0; i < height(); i++) {
+//            for (int j = 0; j < width(); j++) {
+//                StdOut.print(pathsMatrix[i][j] + " ");
+//            }
+//            StdOut.println();
+//        }
+        
+        return pathsMatrix;
+    }
+    
+    // requires bulding pathsMatrix in relaxVertices method before
+    private int[] seam(double[][] pathsMatrix) {
+        Stack<Integer> shortPath = new Stack<Integer>();
+        BST<Double, Integer> lastLinePathsMatrix = new BST<Double, Integer>();
+        int[] seam = new int[height()];
+        for (int j = 0; j < width(); j++) {
+            lastLinePathsMatrix.put(pathsMatrix[height()-1][j], j);
+        }
+        int minColLastLine = lastLinePathsMatrix.get(lastLinePathsMatrix.min());
+//        StdOut.println(minColLastLine);  // DEBUG
+        
+        // Build short path array of indexes representing the seam
+        shortPath.push(minColLastLine);
+//        StdOut.println(shortPath.toString());  // DEBUG
+        int nextCol = minColLastLine;
+        int minCol = 0;
+        double minVal;
+        for (int i = height() - 2; i >= 0; i--) {
+            minVal = Double.POSITIVE_INFINITY;
+            for (int j = nextCol - 1; j <= nextCol + 1; j++) {
+                if (j != -1 && j != width()) {
+                    if (pathsMatrix[i][j] < minVal) {
+                        minVal = pathsMatrix[i][j];
+                        minCol = j;
+                    }
+                }
+            }
+            nextCol = minCol;
+//            StdOut.println(minVal); // DEBUG
+            shortPath.push(minCol);
+//            break; // DEBUG
+//            StdOut.println(shortPath.toString());  // DEBUG
+        }
+        for (int i = 0; i < height(); i++) {
+            seam[i] = shortPath.pop();
+//            StdOut.println(seam[i]);  // DEBUG
+        }
+        
+        return seam;
     }
     
     private double xGradient(int x, int y) {
@@ -122,12 +225,23 @@ public class SeamCarver {
         assert sc.energy(2, 3) == Math.sqrt(21220);
         
         // test energyMatrix
+        sc.setEnergyMatrix();
         assert sc.energyMatrix[0][0] == sc.energy(0, 0);
         assert sc.energyMatrix[0][1] == sc.energy(1, 0);
         assert sc.energyMatrix[0][2] == sc.energy(2, 0);
         assert sc.energyMatrix[2][1] == sc.energy(1, 2);
         assert sc.energyMatrix[2][0] == sc.energy(0, 2);
         assert sc.energyMatrix[3][2] == sc.energy(2, 3);
+        
+        // new SeamCarver
+        Picture pic1 = new Picture("tests/6x5.png");
+        SeamCarver sc1 = new SeamCarver(pic1);
+        // test width's and height's picture
+        assert sc1.width() == 6;
+        assert sc1.height() == 5;
+        // test findVerticalSeam
+        int[] result = {3, 4, 3, 2, 2};
+        assert Arrays.equals(sc1.findVerticalSeam(), result);
     }
 
 }
