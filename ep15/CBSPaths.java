b944bf812b0b9e268aa0bc5a10e1a4937c177472
caco <caco@member.fsf.org>
2017-07-02 12:53:35 -0300

    Find the safe cities

--- ep15/CBSPaths.java
+++ ep15/CBSPaths.java
@@ -2,7 +2,9 @@ import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut; //DEBUG
 import edu.princeton.cs.algs4.EdgeWeightedDigraph;
 import edu.princeton.cs.algs4.DirectedEdge;
+import edu.princeton.cs.algs4.DijkstraSP;
 import edu.princeton.cs.algs4.BST;
+import edu.princeton.cs.algs4.Bag;
 import java.util.Scanner;
 import java.util.Arrays; //DEBUG
 
@@ -10,7 +12,7 @@ public class CBSPaths {
     private EdgeWeightedDigraph ewd;
     private BST<String, Integer> cities;
     private String[] executiveCities;
-    private String mp;
+    private String mpCity;
     
     public CBSPaths(Scanner stdin) {
         int n = stdin.nextInt();
@@ -37,8 +39,48 @@ public class CBSPaths {
         for (int i = 0; i < k; i++)
             executiveCities[i] = stdin.next();
         // read the last line of StdIn representing Ministério Público intial city
-        mp = stdin.next();
+        mpCity = stdin.next();
         
+    }
+    
+    public Bag<String> safeCities() {
+        Bag<String> safeCities = new Bag<String>();
+        // If some executives city is the same as MP city, game over
+        if (Arrays.asList(executiveCities).contains(mpCity))
+            return safeCities;
+        
+        DijkstraSP sp;
+        int mpCityV = cities.get(mpCity);
+        for (String city : cities.keys()) {
+            int cityV = cities.get(city);
+            if (city.equals(mpCity)) continue; // the MP city is not secure by definition
+            
+            // City that CBS executives are before the flies are secure only if
+            // MP city has no edge to it.
+            if (Arrays.asList(executiveCities).contains(city)) {
+                if (mpCityHasEdgeTo(cityV)) continue;
+            }
+            sp = new DijkstraSP(ewd, mpCityV);
+            double mpDistTo = sp.distTo(cityV);
+            for (int i = 0; i < executiveCities.length; i++) {
+                sp = new DijkstraSP(ewd, cities.get(executiveCities[i]));
+                if (sp.distTo(cityV) >= mpDistTo) continue;
+            }
+            safeCities.add(city);
+        }
+        return safeCities;
+    }
+    
+    /**
+     * Private methods
+     */
+    private boolean mpCityHasEdgeTo(int cityV) {
+        for ( DirectedEdge c : ewd.adj(cities.get(mpCity)) ) {
+            if (cityV == c.to()) {
+                return true;
+            }
+        }
+        return false;
     }
     
     public static void main(String args[]) {
@@ -50,13 +92,23 @@ public class CBSPaths {
 //        for (DirectedEdge e : cbs.ewd.edges()) { //DEBUG
 //            StdOut.println(e);
 //        }
+        // executive's cities
         assert Arrays.asList(cbs.executiveCities).contains("POA") == true;
         assert Arrays.asList(cbs.executiveCities).contains("GIG") == true;
         assert Arrays.asList(cbs.executiveCities).contains("GRU") == false;
         assert Arrays.asList(cbs.executiveCities).contains("CNF") == false;
         assert Arrays.asList(cbs.executiveCities).contains("BSB") == false;
         assert Arrays.asList(cbs.executiveCities).contains("CWB") == false;
+        // Ministério Público city
+        assert cbs.mpCity.equals("GRU");
         
-        assert cbs.mp.equals("GRU");
+        // test safe cities
+        String [] safeCities = {"CNF", "BSB", "CWB"};
+        for (String city : cbs.safeCities())
+//            StdOut.println(city); //DEBUG
+            assert Arrays.asList(safeCities).contains(city) == true;
+        assert Arrays.asList(safeCities).contains("POA") == false;
+        assert Arrays.asList(safeCities).contains("GIG") == false;
+        assert Arrays.asList(safeCities).contains("GRU") == false;
     }
 }
