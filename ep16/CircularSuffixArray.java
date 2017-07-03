708114b5a3d3c02bbf1f90fd3d2dd5406cf55666
caco <caco@member.fsf.org>
2017-07-03 20:12:59 -0300

    Implements constructor

--- ep16/CircularSuffixArray.java
+++ ep16/CircularSuffixArray.java
@@ -1,8 +1,10 @@
+import edu.princeton.cs.algs4.StdOut;
+
 public class CircularSuffixArray {
-    private final int [] index;
+    private final String [] index;
     private CircularSuffix cs;
     private class CircularSuffix implements Comparable<CircularSuffix> {
-        private CircularSuffix() {
+        private CircularSuffix(String suffix) {
         }
         public int compareTo(CircularSuffix that) {
             return -1; //implement it!
@@ -11,7 +13,16 @@ public class CircularSuffixArray {
     
     // circular suffix array of s
     public CircularSuffixArray(String s) {
-        index = new int[s.length()];
+        index = new String[s.length()];
+        index[0] = s;
+        StringBuilder sb;
+        for (int i = 1; i < index.length; i++) {
+            sb = new StringBuilder();
+            for (int j = i; j < i + index.length; j++) {
+                sb.append(s.charAt(j % index.length));
+            }
+            index[i] = sb.toString();
+        }
     }
     
     // length of s
@@ -29,5 +40,18 @@ public class CircularSuffixArray {
         // test constructor
         CircularSuffixArray csa = new CircularSuffixArray("ABRACADABRA!");
         assert csa.index.length == 12;
+        assert csa.index[0].equals("ABRACADABRA!");
+        assert csa.index[1].equals("BRACADABRA!A");
+        assert csa.index[2].equals("RACADABRA!AB");
+        assert csa.index[3].equals("ACADABRA!ABR");
+        assert csa.index[4].equals("CADABRA!ABRA");
+        assert csa.index[5].equals("ADABRA!ABRAC");
+        assert csa.index[6].equals("DABRA!ABRACA");
+        assert csa.index[7].equals("ABRA!ABRACAD");
+        assert csa.index[8].equals("BRA!ABRACADA");
+        assert csa.index[9].equals("RA!ABRACADAB");
+        assert csa.index[10].equals("A!ABRACADABR");
+        assert csa.index[11].equals("!ABRACADABRA");
+
     }
 }
