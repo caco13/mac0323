4452830e9d1cca811d6b5dee4eab64753c0be222
caco <caco@member.fsf.org>
2017-07-04 20:06:25 -0300

    Implements CircularSuffix static class

--- ep16/CircularSuffixArray.java
+++ ep16/CircularSuffixArray.java
@@ -1,13 +1,25 @@
-import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdOut;  // DEBUG
+import edu.princeton.cs.algs4.LSD;
 
 public class CircularSuffixArray {
     private final String [] index;
     private CircularSuffix cs;
-    private class CircularSuffix implements Comparable<CircularSuffix> {
-        private CircularSuffix(String suffix) {
+    private static class CircularSuffix implements Comparable<CircularSuffix> {
+        // do not instantiate
+        private CircularSuffix() { }
+        
+        public static int getIthSortedSuffix(String [] suffix, int i) {
+            String [] circularSuffix = new String[suffix.length];
+            System.arraycopy(suffix, 0, circularSuffix, 0, suffix.length);
+            LSD.sort(circularSuffix, circularSuffix.length);
+            for (int j = 0; j < suffix.length; j++) {
+                if (circularSuffix[i].equals(suffix[j]))
+                    return j;
+            }
+            return -1; // Something went wrong
         }
         public int compareTo(CircularSuffix that) {
-            return -1; //implement it!
+            return -1; //implement it! Será?
         };
     }
     
@@ -32,7 +44,7 @@ public class CircularSuffixArray {
     
     // returns index of ith sorted suffix
     public int index(int i) {
-        return -1; //implement it!
+        return CircularSuffix.getIthSortedSuffix(index, i);
     }
     
     // unit testing (not graded)
@@ -52,6 +64,27 @@ public class CircularSuffixArray {
         assert csa.index[9].equals("RA!ABRACADAB");
         assert csa.index[10].equals("A!ABRACADABR");
         assert csa.index[11].equals("!ABRACADABRA");
+        
         assert csa.length() == csa.index.length;
+        
+        // test index method
+        assert csa.index(0) == 11;
+        assert csa.index(1) == 10;
+        assert csa.index(2) == 7;
+        assert csa.index(3) == 0;
+        assert csa.index(4) == 3;
+        assert csa.index(5) == 5;
+        assert csa.index(6) == 8;
+        assert csa.index(7) == 1;
+        assert csa.index(8) == 4;
+        assert csa.index(9) == 6;
+        assert csa.index(10) == 9;
+        assert csa.index(11) == 2;
+        
+        // test index method with longer word
+        CircularSuffixArray csa1 = new CircularSuffixArray("INCONSTITUCIONABILISSIMAMENTE!");
+        assert csa1.index(0) == 29;
+        assert csa1.index(1) == 14;
+        
     }
 }
