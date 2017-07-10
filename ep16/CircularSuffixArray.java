b38e0a95b4385d7f921cbe5c9fa1d9eae825dbdc
caco <caco@member.fsf.org>
2017-07-10 19:28:59 -0300

    Implements CircularSuffixArray class

--- ep16/CircularSuffixArray.java
+++ ep16/CircularSuffixArray.java
@@ -1,71 +1,60 @@
 import edu.princeton.cs.algs4.StdOut;  // DEBUG
-import edu.princeton.cs.algs4.LSD;
+import java.util.Arrays;
 
 public class CircularSuffixArray {
-    private final String [] index;
-    private CircularSuffix cs;
-    private static class CircularSuffix implements Comparable<CircularSuffix> {
-        // do not instantiate
-        private CircularSuffix() { }
-        
-        public static int getIthSortedSuffix(String [] suffix, int i) {
-            String [] circularSuffix = new String[suffix.length];
-            System.arraycopy(suffix, 0, circularSuffix, 0, suffix.length);
-            LSD.sort(circularSuffix, circularSuffix.length);
-            for (int j = 0; j < suffix.length; j++) {
-                if (circularSuffix[i].equals(suffix[j]))
-                    return j;
-            }
-            return -1; // Something went wrong
-        }
-        public int compareTo(CircularSuffix that) {
-            return -1; //implement it! Será?
-        };
+    private CircularSuffix[] circularSuffixes;
+    
+    public CircularSuffixArray(String text) {
+        int n = text.length();
+        this.circularSuffixes = new CircularSuffix[n];
+        for (int i = 0; i < n; i++)
+            circularSuffixes[i] = new CircularSuffix(text, i);
+        Arrays.sort(circularSuffixes);
     }
     
-    // circular suffix array of s
-    public CircularSuffixArray(String s) {
-        index = new String[s.length()];
-        index[0] = s;
-        StringBuilder sb;
-        for (int i = 1; i < index.length; i++) {
-            sb = new StringBuilder();
-            for (int j = i; j < i + index.length; j++) {
-                sb.append(s.charAt(j % index.length));
+    private static class CircularSuffix implements Comparable<CircularSuffix> {
+        private final String text;
+        private final int index;
+        
+        private CircularSuffix(String text, int index) {
+            this.text = text;
+            this.index = index;
+        }
+        
+        private int length() {
+            return text.length() - index;
+        }
+        
+        private char charAt(int i) {
+            return text.charAt(index + i);
+        }
+       
+        public int compareTo(CircularSuffix that) {
+            if (this == that) return 0;
+            int n = Math.min(this.length(), that.length());
+            for (int i = 0; i < n; i++) {
+                if (this.charAt(i) < that.charAt(i)) return -1;
+                if (this.charAt(i) > that.charAt(i)) return +1;
             }
-            index[i] = sb.toString();
+            return this.length() - that.length();
         }
     }
     
     // length of s
     public int length() {
-        return index.length;
+        return circularSuffixes.length;
     }
     
     // returns index of ith sorted suffix
     public int index(int i) {
-        return CircularSuffix.getIthSortedSuffix(index, i);
+        if (i < 0 || i >= circularSuffixes.length) throw new IllegalArgumentException();
+        return circularSuffixes[i].index;
     }
     
     // unit testing (not graded)
     public static void main(String[] args) {
         // test constructor
         CircularSuffixArray csa = new CircularSuffixArray("ABRACADABRA!");
-        assert csa.index.length == 12;
-        assert csa.index[0].equals("ABRACADABRA!");
-        assert csa.index[1].equals("BRACADABRA!A");
-        assert csa.index[2].equals("RACADABRA!AB");
-        assert csa.index[3].equals("ACADABRA!ABR");
-        assert csa.index[4].equals("CADABRA!ABRA");
-        assert csa.index[5].equals("ADABRA!ABRAC");
-        assert csa.index[6].equals("DABRA!ABRACA");
-        assert csa.index[7].equals("ABRA!ABRACAD");
-        assert csa.index[8].equals("BRA!ABRACADA");
-        assert csa.index[9].equals("RA!ABRACADAB");
-        assert csa.index[10].equals("A!ABRACADABR");
-        assert csa.index[11].equals("!ABRACADABRA");
-        
-        assert csa.length() == csa.index.length;
         
         // test index method
         assert csa.index(0) == 11;
@@ -80,11 +69,5 @@ public class CircularSuffixArray {
         assert csa.index(9) == 6;
         assert csa.index(10) == 9;
         assert csa.index(11) == 2;
-        
-        // test index method with longer word
-        CircularSuffixArray csa1 = new CircularSuffixArray("INCONSTITUCIONABILISSIMAMENTE!");
-        assert csa1.index(0) == 29;
-        assert csa1.index(1) == 14;
-        
     }
 }
