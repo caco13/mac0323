c89c0266ab29fa7512d9fe6bd244850b04eccd9c
caco <caco@member.fsf.org>
2017-07-10 20:14:58 -0300

    Implements BurrowsWheeler transform method

--- ep16/BurrowsWheeler.java
+++ ep16/BurrowsWheeler.java
@@ -1,6 +1,24 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 public class BurrowsWheeler {
     // apply Burrows-Wheeler transform, reading from standard input and writing to standard output
     public static void transform() {
+        String s;
+        while (!StdIn.isEmpty()) {
+            s = StdIn.readString();
+            CircularSuffixArray csa = new CircularSuffixArray(s);
+            StringBuilder lastCharString = new StringBuilder(0);
+            for (int i = 0; i < s.length(); i++) {
+                if (csa.index(i) == 0) {
+                    StdOut.println(i);
+                    lastCharString.append(s.charAt(s.length() - 1));
+                    continue;
+                }
+                lastCharString.append(s.charAt(csa.index(i) - 1));
+            }
+            StdOut.println(lastCharString);
+        }
     }
 
     // apply Burrows-Wheeler inverse transform, reading from standard input and writing to standard output
@@ -10,5 +28,9 @@ public class BurrowsWheeler {
     // if args[0] is '-', apply Burrows-Wheeler transform
     // if args[0] is '+', apply Burrows-Wheeler inverse transform
     public static void main(String[] args) {
+        if (args[0].equals("-"))
+            BurrowsWheeler.transform();
+        if (args[0].equals("+"))
+            BurrowsWheeler.inverseTransform();
     }
 }
